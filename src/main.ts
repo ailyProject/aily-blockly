c8e0cbbc32563e5e1b7a54bd47d054a4fb07995f
coloz <coloz999@gmail.com>
2025-09-24 22:11:34 +0800

    移除Monaco Editor configuration

--- src/main.ts
+++ src/main.ts
@@ -6,23 +6,7 @@ import { AppComponent } from './app/app.component';
 import './app/utils/global-chat.utils';
 
 // Monaco Editor configuration
-self.MonacoEnvironment = {
-  getWorkerUrl: function (moduleId, label) {
-    if (label === 'json') {
-      return './assets/vs/language/json/json.worker.js';
-    }
-    if (label === 'css' || label === 'scss' || label === 'less') {
-      return './assets/vs/language/css/css.worker.js';
-    }
-    if (label === 'html' || label === 'handlebars' || label === 'razor') {
-      return './assets/vs/language/html/html.worker.js';
-    }
-    if (label === 'typescript' || label === 'javascript') {
-      return './assets/vs/language/typescript/ts.worker.js';
-    }
-    return './assets/vs/editor/editor.worker.js';
-  }
-};
+self.MonacoEnvironment = {};
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
